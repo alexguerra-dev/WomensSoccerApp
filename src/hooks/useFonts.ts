3ba43a6b8916080655c17630393992d6de331341
Alexander Guerra <alexander.a.guerra@gmail.com>
2025-12-28 19:04:41 -0600

    refactor: add type annotations to useFonts hook

--- src/hooks/useFonts.ts
+++ src/hooks/useFonts.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
 
-export default function useFonts() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+export default function useFonts(): boolean {
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     async function loadFonts() {
+      console.log("Loading fonts...");
       await Font.loadAsync({
         "OpenDyslexic-Regular": require("../../assets/fonts/OpenDyslexic-Regular.otf"),
         "OpenDyslexic-Bold": require("../../assets/fonts/OpenDyslexic-Bold.otf"),
